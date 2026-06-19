0b274b714fa881f5a47da6e0affe1a8934c86db1
joy <ftmorolfo@gmail.com>
2026-06-19 12:03:15 +0800

    stackFull/stackEmpty function

--- MCO1-main.c
+++ MCO1-main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
+
+bool stackFull(int top) { //????
+    bool result;
+    if(top == 0)
+        result = true;
+    else
+        result = false;
+
+    return result;
+}
+
+// char operatorTop(char* Infix) {
+//     return ;
+// }
+
+// int operandTop() {
+//     return ;
+// }
 
 void push(char* Infix, char* input){
 	int size = strlen(Infix);
@@ -11,14 +30,15 @@ void push(char* Infix, char* input){
 
 int main(){
 	char Infix[256]; //string array with 256 chars
-	
+	int top = -1; // stack empty
+
 
 	printf("Input: ");
-	
-	//stores the inputted infix in the array 
+
+	//stores the inputted infix in the array
 	//fgets(Infix, sizeof(Infix), stdin);
 	scanf("%[^\n]", Infix);
 	char input[] = "-"; //test
 	push(Infix, input);
-	
+
 }
